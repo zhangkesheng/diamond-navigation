b0ce6cb758a4c63f41b7d95634242f2669385aad
zhangkesheng <1307716475@qq.com>
2020-12-24 10:44:23 +0800

    :beetle: Fix week error

--- src/components/time.tsx
+++ src/components/time.tsx
@@ -4,21 +4,25 @@ import React from 'react';
 
 interface TimeState {
   time: moment.Moment;
+  timer: NodeJS.Timeout;
 }
 
-const week = ['一', '二', '三', '四', '五', '六', '日'];
+const week = ['日', '一', '二', '三', '四', '五', '六'];
 
 export default class timeComp extends React.Component<{}, TimeState> {
   constructor(props: {}) {
     super(props);
     this.state = {
       time: moment(),
+      timer: setInterval(() => {
+        this.setState({
+          time: moment(),
+        });
+      }, 10000),
     };
-    setInterval(() => {
-      this.setState({
-        time: moment(),
-      });
-    }, 10000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.state.timer);
   }
   render() {
     const { time } = this.state;
@@ -28,7 +32,7 @@ export default class timeComp extends React.Component<{}, TimeState> {
           {time.format('HH:mm')}
         </Avatar>
         <Avatar size={50} style={{ backgroundColor: 'rgb(0, 0, 0,.3)' }}>
-          周{week[Number(time.format('E'))]}
+          周{week[Number(time.format('d'))]}
         </Avatar>
       </Carousel>
     );
